2f21d448779c594a042dc56d25259736c67b54fc
Sultan Beksultanov <sultanbek9899@gmail.com>
2022-05-24 20:20:25 +0600

    context

--- backend/apps/cart/cart_context.py
+++ backend/apps/cart/cart_context.py
@@ -3,6 +3,6 @@ from backend.apps.cart.cart import Cart
 
 def get_cart(request):
     cart = Cart(request)
-    for i in cart:
-        print(i)
+    # for i in cart:
+    #     print(i)
     return {"cart":cart}
